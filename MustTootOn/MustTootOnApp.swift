a3b0b20f795cfee9648607c65a6d8743454a543b
Willy Breitenbach <willybr@me.com>
2022-11-04 18:53:37 +0100

    Initial Commit

---
+++ MustTootOn/MustTootOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MustTootOnApp.swift
+//  MustTootOn
+//
+//  Created by Willy Breitenbach on 04.11.22.
+//
+
+import SwiftUI
+
+@main
+struct MustTootOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MustTootOnUITests/MustTootOnUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MustTootOnUITestsLaunchTests.swift
+//  MustTootOnUITests
+//
+//  Created by Willy Breitenbach on 04.11.22.
+//
+
+import XCTest
+
+final class MustTootOnUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
